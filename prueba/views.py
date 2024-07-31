3f381857bfec3114aa89d8bfb152210b7c6ef7b3
ahinestrog <ahinestrog@eafit.edu.co>
2024-07-31 13:15:45 -0500

    busqueda de peliculas

--- prueba/views.py
+++ prueba/views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Prueba
+
 # Create your views here.
 def home(request):
     #return HttpResponse('<h1>Welcome home page</h1>')
     #return render(request,'home.html')
-    return render(request,'home.html',{'name':'Alejandro Hinestroza'})
+    #return render(request,'home.html',{'name':'Alejandro Hinestroza'})
+    searchTerm = request.GET.get('searchMovie')
+    if searchTerm:
+        movies = Prueba.objects.filter(title__icontains=searchTerm)
+    else:
+        movies = Prueba.objects.all()
+    return render(request,'home.html',{'searchTerm':searchTerm,'movies':movies})
+
+
 def about(request):
-    return HttpResponse('<h1>About page</h1>')
+    #return HttpResponse('<h1>About page</h1>')
+    return render(request,'about.html')
